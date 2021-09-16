a3b2db56ab936163b2d623ea04f43e2143d480bd
Gabriel Antunes <gabriel.antunes@compasso.com.br>
2021-09-16 09:56:02 -0300

    how to declare vars

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Olá Mundo!")
+	var name string
+	var age float32
+	version := 1.1 // declarando variavel e atribuindo um valor
+
+	fmt.Println("-> Endereço de idade", &age)
+	fmt.Println("-> Endereço de nome", &name)
+
+	fmt.Println("- Olá, tudo bem? Por favor digite seu nome:")
+	fmt.Scan(&name)
+
+	fmt.Println("- Prazer", name, "digite agora sua idade:")
+	fmt.Scan(&age)
+
+	fmt.Println("- Seu nome é", name, " e sua idade é ", age, "anos")
+
+	fmt.Println("--- Hello Word em Go v", version, "---")
+	fmt.Println("Tipo inferido a version: ", reflect.TypeOf(version))
 }
